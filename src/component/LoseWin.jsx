52ef1e54ed8ae7acc3993957bb72d63c9a4bdc73
afifi brahim <afifi71brahim@gmail.com>
2023-04-05 03:19:24 +0000

    fix bug 2

--- src/component/LoseWin.jsx
+++ src/component/LoseWin.jsx
@@ -11,7 +11,7 @@ const LoseWin = ({winlose}) => {
            
            
     </div>
-      <div className='w-44  flex-col mt-10 items-center justify-center gap-4 mx-4 flex sm:hidden absolute -bottom-48 -translate-x-20'>
+      <div className='w-44  flex-col mt-10 items-center justify-center gap-4 mx-4 flex sm:hidden absolute -bottom-32 -translate-x-20'>
           <h2 className='uppercase tracking-[3px] text-white text-2xl text-center  font-bold'>{winlose}</h2>
           <Link className='w-full' to={'/'}>
            <button className='w-full  py-3 rounded-md uppercase text-[11px] text-red-500 bg-white font-semibold tracking-[2px]'>play again </button>
